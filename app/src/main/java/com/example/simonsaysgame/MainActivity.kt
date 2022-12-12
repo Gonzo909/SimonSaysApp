f9f7c3397b4318514a68c493853f827b41b96f7d
Gonzo909 <gonzalo90952@gmail.com>
2022-12-12 12:49:49 -0700

    updated mainactivity code

--- app/src/main/java/com/example/simonsaysgame/MainActivity.kt
+++ app/src/main/java/com/example/simonsaysgame/MainActivity.kt
@@ -44,96 +44,133 @@ fun play(yButton : Button, bButton : Button, rButton : Button, gButton : Button)
     val playerOrder = mutableListOf<String>()
     var level = 1
 
-    //until the player loses
-    while (win) {
-        //println("Simon Says...")
-        for (i in 1..level) {
-            //only adds to list when iterator is at the end of the list
-            if (i > buttonOrder.size) {
-                //chooses a number which represents a color
-                val randomColor = (1..4).random()
-
-                //adds a color to the list that the player has to match
-                when (randomColor) {
-                    1 -> {
-                        buttonOrder.add("red")
-                        } //add this string
-                    2 -> {
-                        buttonOrder.add("blue")
-                    }
-                    3 -> {
-                        buttonOrder.add("yellow")
-                    }
-                    4 -> {
-                        buttonOrder.add("green")
-                    }
-                }
-            }
+    val randomColor = (1..4).random()
+    when (randomColor) {
+        1 -> {
+            buttonOrder.add("red")
+        } //add this string
+        2 -> {
+            buttonOrder.add("blue")
         }
-
-        for (button in buttonOrder) {
-            when(button) {
-                "red" -> {
-                    rButton.performLongClick()
-                    Thread.sleep(500)
-                }
-                "blue" -> {
-                    bButton.performLongClick()
-                    Thread.sleep(500)
-                }
-                "yellow" -> {
-                    yButton.performLongClick()
-                    Thread.sleep(500)
-                }
-                "green" -> {
-                    gButton.performLongClick()
-                    Thread.sleep(500)
-                }
-            }
+        3 -> {
+            buttonOrder.add("yellow")
         }
-
-        //Toast.makeText(this, "Your Turn!", Toast.LENGTH_SHORT).show()
-
-        //get player's button guesses
-        playerOrder.clear()
-        for (i in 1..level) {
-            //when player hits a button, add button to list of player's button order
-            rButton.setOnClickListener {
-                playerOrder.add("red")
-            }
-            bButton.setOnClickListener {
-                playerOrder.add("blue")
-            }
-            yButton.setOnClickListener {
-                playerOrder.add("yellow")
-            }
-            gButton.setOnClickListener {
-                playerOrder.add("green")
-            }
+        4 -> {
+            buttonOrder.add("green")
         }
-
-        //iterates through comp's button order and player's and compares to see if player wins level
-        for (i in 1..buttonOrder.size) {
-            //if player's button order does not match the CPU's
-            if (playerOrder[i-1] != buttonOrder[i-1]) {
-                //end game
-                win = false
-                break
-            }
-        }
-
-        //if player lost, game over
-        if (!win) {
-            //Toast.makeText(this, "Game Over! Lost at level: $level", Toast.LENGTH_SHORT).show()
-
-            //adds the current level to the high score list, then sorts it
-            //highScores.add(level)
-            //highScores.sort()
-            return
-        }
-
-        //else, increase level and play again
-        level += 1
     }
 
+    //get player's button guesses
+    playerOrder.clear()
+    //when player hits a button, add button to list of player's button order
+    rButton.setOnClickListener {
+        playerOrder.add("red")
+        if (playerOrder.size == buttonOrder.size) {
+            for (i in 0..playerOrder.size-1){
+                if (playerOrder[i] != buttonOrder[i]){
+                    win = false
+                    break
+                }
+            }
+            level++
+            cpuTurn(level,buttonOrder,rButton,bButton,yButton,gButton)
+        }
+    }
+    bButton.setOnClickListener {
+        playerOrder.add("blue")
+        if (playerOrder.size == buttonOrder.size) {
+            for (i in 0..playerOrder.size-1){
+                if (playerOrder[i] != buttonOrder[i]){
+                    win = false
+                    break
+                }
+            }
+            level++
+            cpuTurn(level,buttonOrder,rButton,bButton,yButton,gButton)
+        }
+    }
+    yButton.setOnClickListener {
+        playerOrder.add("yellow")
+        if (playerOrder.size == buttonOrder.size) {
+            for (i in 0..playerOrder.size-1){
+                if (playerOrder[i] != buttonOrder[i]){
+                    win = false
+                    break
+                }
+            }
+            level++
+            cpuTurn(level,buttonOrder,rButton,bButton,yButton,gButton)
+        }
+    }
+    gButton.setOnClickListener {
+        playerOrder.add("green")
+        if (playerOrder.size == buttonOrder.size) {
+            for (i in 0..playerOrder.size-1){
+                if (playerOrder[i] != buttonOrder[i]){
+                    win = false
+                    break
+                }
+            }
+            level++
+            cpuTurn(level,buttonOrder,rButton,bButton,yButton,gButton)
+        }
+    }
+
+    //if player lost, game over
+    if (!win) {
+        //Toast.makeText(this, "Game Over! Lost at level: $level", Toast.LENGTH_SHORT).show()
+        //adds the current level to the high score list, then sorts it
+        //highScores.add(level)
+        //highScores.sort()
+        return
+    }
+
+
+}
+
+fun cpuTurn(level : Int, buttonOrder : MutableList<String>, rButton : Button, bButton : Button, yButton : Button, gButton : Button) {
+    for (i in 1..level) {
+        //only adds to list when iterator is at the end of the list
+        if (i > buttonOrder.size) {
+            //chooses a number which represents a color
+            val randomColor = (1..4).random()
+
+            //adds a color to the list that the player has to match
+            when (randomColor) {
+                1 -> {
+                    buttonOrder.add("red")
+                } //add this string
+                2 -> {
+                    buttonOrder.add("blue")
+                }
+                3 -> {
+                    buttonOrder.add("yellow")
+                }
+                4 -> {
+                    buttonOrder.add("green")
+                }
+            }
+        }
+    }
+
+    for (button in buttonOrder) {
+        when(button) {
+            "red" -> {
+                rButton.performLongClick()
+                Thread.sleep(500)
+            }
+            "blue" -> {
+                bButton.performLongClick()
+                Thread.sleep(500)
+            }
+            "yellow" -> {
+                yButton.performLongClick()
+                Thread.sleep(500)
+            }
+            "green" -> {
+                gButton.performLongClick()
+                Thread.sleep(500)
+            }
+        }
+    }
 }
